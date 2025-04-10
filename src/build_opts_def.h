9d476df8f35666d5fbb0fd6e2f48d3b7636a753c
Josh <jsimonot@live.ca>
2025-04-10 00:19:02 -0400

    add new opt defs field: ccstr_init_cp
    this member will decide how to the opt gets innitialized for each target

--- src/build_opts_def.h
+++ src/build_opts_def.h
@@ -10,44 +10,51 @@
  #define _BUILD_OPT_DEFS_H_
 
  #include <stdbool.h>
+ #include <stdio.h>
+
+// provides a "definition" of each build options which allows
+// to declaritively define how to parse and handle each option
+// and provides the infrastructure for looping over each opt
+struct option_def{
+    const char *name;
+    bool supports_append;
+    bool ccstr_init_cp;
+    int field_offset;
+    void (*opt_handler)(const struct option_def *def, void *optptr, const char *key, const char *value);
+};
 
  static void general_opt_handler(const struct option_def *def, void *optptr, const char *key, const char *value);
  static void so_version_opt_handler(const struct option_def *def, void *optptr, const char *key, const char *value);
  static void type_opt_handler(const struct option_def *def, void *optptr, const char *key, const char *value);
 
-// provides a "definition" of each build options which allows
-// to declaritively define how to parse and handle each option
-// and provides the infrastructure for looping over each opt
- struct option_def{
-    const char *name;
-    bool supports_append;
-    int field_offset;
-    void (*opt_handler)(const struct option_def *def, void *optptr, const char *key, const char *value);
+static const struct option_def build_option_defs[] = {
+    {"BUILD_ROOT",   false, true, offsetof(struct build_opts, build_root), general_opt_handler},
+    {"INSTALL_ROOT", false, true, offsetof(struct build_opts, install_root), general_opt_handler},
+    {"CC",           false, true, offsetof(struct build_opts, cc), general_opt_handler},
+    {"LIBNAME",      false, true, offsetof(struct build_opts, libname), general_opt_handler},
+    {"SRC_PATHS",    true, true,  offsetof(struct build_opts, srcpaths), general_opt_handler},
+    {"INC_PATHS",    true, true,  offsetof(struct build_opts, incpaths), general_opt_handler},
+    {"LIB_PATHS",    true, true,  offsetof(struct build_opts, libpaths), general_opt_handler},
+    {"CCFLAGS",      true, true,  offsetof(struct build_opts, ccflags), general_opt_handler},
+    {"LDFLAGS",      true, true,  offsetof(struct build_opts, ldflags), general_opt_handler},
+    {"LIBS",         true, true,  offsetof(struct build_opts, libs), general_opt_handler},
+    {"RELEASE",      true, true,  offsetof(struct build_opts, release), general_opt_handler},
+    {"DEBUG",        true, true,  offsetof(struct build_opts, debug), general_opt_handler},
+    {"COMPILE",      false, true,  offsetof(struct build_opts, compile), general_opt_handler},
+    {"LINK",         false, true,  offsetof(struct build_opts, link), general_opt_handler},
+    {"LINK_SHARED",  false, true,  offsetof(struct build_opts, link_shared), general_opt_handler},
+    {"LINK_STATIC",  false, true,  offsetof(struct build_opts, link_static), general_opt_handler},
+    {"INSTALL_DIR",  false, true, offsetof(struct build_opts, installdir), general_opt_handler},
+    {"TARGET",       false, false, offsetof(struct build_opts, target), general_opt_handler},
+    {"TYPE",         false, false, offsetof(struct build_opts, type), type_opt_handler},
+    {"SO_VERSION",   false, false, offsetof(struct build_opts, so_version), so_version_opt_handler},
+    {NULL, 0, 0, 0, NULL}
 };
 
-static const struct option_def build_option_defs[] = {
-    {"BUILD_ROOT",   false, offsetof(struct build_opts, build_root), general_opt_handler},
-    {"INSTALL_ROOT", false, offsetof(struct build_opts, install_root), general_opt_handler},
-    {"CC",           false, offsetof(struct build_opts, cc), general_opt_handler},
-    {"LIBNAME",      false, offsetof(struct build_opts, libname), general_opt_handler},
-    {"SRC_PATHS",    true,  offsetof(struct build_opts, srcpaths), general_opt_handler},
-    {"INC_PATHS",    true,  offsetof(struct build_opts, incpaths), general_opt_handler},
-    {"LIB_PATHS",    true,  offsetof(struct build_opts, libpaths), general_opt_handler},
-    {"CCFLAGS",      true,  offsetof(struct build_opts, ccflags), general_opt_handler},
-    {"LDFLAGS",      true,  offsetof(struct build_opts, ldflags), general_opt_handler},
-    {"LIBS",         true,  offsetof(struct build_opts, libs), general_opt_handler},
-    {"RELEASE",      true,  offsetof(struct build_opts, release), general_opt_handler},
-    {"DEBUG",        true,  offsetof(struct build_opts, debug), general_opt_handler},
-    {"COMPILE",      false,  offsetof(struct build_opts, compile), general_opt_handler},
-    {"LINK",         false,  offsetof(struct build_opts, link), general_opt_handler},
-    {"LINK_SHARED",  false,  offsetof(struct build_opts, link_shared), general_opt_handler},
-    {"LINK_STATIC",  false,  offsetof(struct build_opts, link_static), general_opt_handler},
-    {"INSTALL_DIR",  false, offsetof(struct build_opts, installdir), general_opt_handler},
-    {"TARGET",       false, offsetof(struct build_opts, target), general_opt_handler},
-    {"TYPE",         false, offsetof(struct build_opts, type), type_opt_handler},
-    {"SO_VERSION",   false, offsetof(struct build_opts, so_version), so_version_opt_handler},
-    {NULL, 0, 0, NULL}
-};
+static inline
+bool append_opt(const char *optname) {
+    return optname[strlen(optname)-1] == '+';
+}
 
 // handles general case for parsed options
 // if append is true, append value to existing opt
