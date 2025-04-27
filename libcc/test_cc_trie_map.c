967756c6dab82252f039c9c1d91647f5d299b1cb
Josh <jsimonot@live.ca>
2025-04-26 20:53:10 -0400

    use test header checks instead of asserts

--- libcc/test_cc_trie_map.c
+++ libcc/test_cc_trie_map.c
@@ -13,6 +13,7 @@
 #include "cc_allocator.h"
 
 #include <stdio.h>
+#include "libcc/cc_test.h"
 
 struct iterate_ctx {
     int sum;
@@ -30,134 +31,143 @@ int test_iterate_callback(void *ctx, void *usrdata) {
     return 0;
 }
 
-void test_insert_delete(void) {
+int test_insert_delete(void) {
     struct cc_trie trie = {0};
 
     char usr_test_val0[] = "helloworld";
     char usr_test_val1[] = "hello";
-    char usr_test_val2[] = {0, 255, 128};
+    uint8_t usr_test_val2[] = {0, 255, 128};
 
-    assert(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), usr_test_val0) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), usr_test_val1) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), usr_test_val2) == 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), usr_test_val0), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), usr_test_val1), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), usr_test_val2), 0);
 
-    assert(cc_trie_delete(&trie, usr_test_val0, strlen(usr_test_val0)) == 0);
-    assert(cc_trie_delete(&trie, usr_test_val1, strlen(usr_test_val1)) == 0);
-    assert(cc_trie_delete(&trie, usr_test_val2, sizeof(usr_test_val2)) == 0);
+    CHKEQ_INT(cc_trie_delete(&trie, usr_test_val0, strlen(usr_test_val0)), 0);
+    CHKEQ_INT(cc_trie_delete(&trie, usr_test_val1, strlen(usr_test_val1)), 0);
+    CHKEQ_INT(cc_trie_delete(&trie, usr_test_val2, sizeof(usr_test_val2)), 0);
     
-    assert(cc_trie_delete(&trie, "123", 3) == ENOENT);
-    assert(cc_trie_delete(&trie, usr_test_val0, strlen(usr_test_val0)) == ENOENT);
-    assert(cc_trie_delete(&trie, usr_test_val1, strlen(usr_test_val1)) == ENOENT);
+    CHKEQ_INT(cc_trie_delete(&trie, "123", 3), ENOENT);
+    CHKEQ_INT(cc_trie_delete(&trie, usr_test_val0, strlen(usr_test_val0)), ENOENT);
+    CHKEQ_INT(cc_trie_delete(&trie, usr_test_val1, strlen(usr_test_val1)), ENOENT);
 
     cc_trie_clear(&trie);
+    return 0;
 }
 
-void test_search(void) {
+int test_search(void) {
     struct cc_trie trie = {0};
 
     char usr_test_val0[] = "helloworld";
     char usr_test_val1[] = "hello";
-    char usr_test_val2[] = {0, 255, 128};
+    uint8_t usr_test_val2[] = {0, 255, 128};
 
-    assert(cc_trie_search(&trie, "123", 3) == NULL);
+    CHKEQ_PTR(cc_trie_search(&trie, "123", 3), NULL);
 
-    assert(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), usr_test_val0) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), usr_test_val1) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), usr_test_val2) == 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), usr_test_val0), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), usr_test_val1), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), usr_test_val2), 0);
 
-    assert(cc_trie_search(&trie, usr_test_val0, strlen(usr_test_val0)) == usr_test_val0);
-    assert(cc_trie_search(&trie, usr_test_val1, strlen(usr_test_val1)) == usr_test_val1);
-    assert(cc_trie_search(&trie, usr_test_val2, sizeof(usr_test_val2)) == usr_test_val2);
+    CHKEQ_PTR(cc_trie_search(&trie, usr_test_val0, strlen(usr_test_val0)), usr_test_val0);
+    CHKEQ_PTR(cc_trie_search(&trie, usr_test_val1, strlen(usr_test_val1)), usr_test_val1);
+    CHKEQ_PTR(cc_trie_search(&trie, usr_test_val2, sizeof(usr_test_val2)), usr_test_val2);
 
-    assert(cc_trie_search(&trie, "hell", 4) == NULL);
-    assert(cc_trie_search(&trie, "hellow", 6) == NULL);
+    CHKEQ_PTR(cc_trie_search(&trie, "hell", 4), NULL);
+    CHKEQ_PTR(cc_trie_search(&trie, "hellow", 6), NULL);
 
-    assert(cc_trie_delete(&trie, usr_test_val0, strlen(usr_test_val0)) == 0);
-    assert(cc_trie_search(&trie, usr_test_val0, strlen(usr_test_val0)) == NULL);
+    CHKEQ_INT(cc_trie_delete(&trie, usr_test_val0, strlen(usr_test_val0)), 0);
+    CHKEQ_PTR(cc_trie_search(&trie, usr_test_val0, strlen(usr_test_val0)), NULL);
 
     cc_trie_clear(&trie);
+    return 0;
 }
 
-void test_iterate(void) {
+int test_iterate(void) {
     struct cc_trie trie = {0};
 
     char usr_test_val0[] = "helloworld";
     char usr_test_val1[] = "hello";
-    char usr_test_val2[] = {0, 255, 128};
+    uint8_t usr_test_val2[] = {0, 255, 128};
 
-    assert(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), (void*)2) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), (void*)4) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), (void*)8) == 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), (void*)2), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), (void*)4), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), (void*)8), 0);
 
     struct iterate_ctx ctx = {
         .sum = 0,
         .count = 0,
     };
-    assert(cc_trie_iterate(&trie, &ctx, test_iterate_callback) == 0);
-    assert(ctx.count == 3);
-    assert(ctx.sum == 14);
+    CHKEQ_INT(cc_trie_iterate(&trie, &ctx, test_iterate_callback), 0);
+    CHKEQ_INT(ctx.count, 3);
+    CHKEQ_INT(ctx.sum, 14);
 
     cc_trie_clear(&trie);
+    return 0;
 }
 
-void test_iterate_exit_early(void) {
+int test_iterate_exit_early(void) {
     struct cc_trie trie = {0};
 
     char usr_test_val0[] = "helloworld";
     char usr_test_val1[] = "hello";
-    char usr_test_val2[] = {0, 255, 128};
+    uint8_t usr_test_val2[] = {0, 255, 128};
 
-    assert(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), (void*)2) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), (void*)42) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), (void*)8) == 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), (void*)2), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), (void*)42), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), (void*)8), 0);
 
     struct iterate_ctx ctx = {
         .sum = 0,
         .count = 0,
     };
-    assert(cc_trie_iterate(&trie, &ctx, test_iterate_callback) == -1);
+    CHKEQ_INT(cc_trie_iterate(&trie, &ctx, test_iterate_callback), -1);
     assert(ctx.count < 3);
     assert(ctx.sum < 14);
 
     cc_trie_clear(&trie);
+    return 0;
 }
 
-void test_extern_allocator(void) {
+int test_extern_allocator(void) {
     struct cc_arena *arena = cc_new_arena_bump_allocator(6*4096);
     struct cc_trie trie = {
         .arena = arena,
     };
     char usr_test_val0[] = "helloworld";
-    assert(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), usr_test_val0) == 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), usr_test_val0), 0);
 
     cc_trie_clear(&trie);
     cc_free_all(arena);
+    return 0;
 }
 
-void test_clear(void) {
+int test_clear(void) {
     struct cc_trie trie = {0};
 
     char usr_test_val0[] = "helloworld";
     char usr_test_val1[] = "hello";
-    char usr_test_val2[] = {0, 255, 128};
+    uint8_t usr_test_val2[] = {0, 255, 128};
 
-    assert(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), usr_test_val0) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), usr_test_val1) == 0);
-    assert(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), usr_test_val2) == 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val0, strlen(usr_test_val0), usr_test_val0), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val1, strlen(usr_test_val1), usr_test_val1), 0);
+    CHKEQ_INT(cc_trie_insert(&trie, usr_test_val2, sizeof(usr_test_val2), usr_test_val2), 0);
 
     cc_trie_clear(&trie);
 
     assert(trie.arena != NULL);
-    assert(trie.root == NULL);
+    CHKEQ_PTR(trie.root, NULL);
+    return 0;
 }
 
 int main(void) {
-    test_insert_delete();
-    test_search();
-    test_iterate();
-    test_iterate_exit_early();
-    test_extern_allocator();
-    test_clear();
-    printf("[PASSED] test trie map\n");
+    int err = 0;
+
+    err |= test_insert_delete();
+    err |= test_search();
+    err |= test_iterate();
+    err |= test_iterate_exit_early();
+    err |= test_extern_allocator();
+    err |= test_clear();
+
+    printf("[%s] test cc_trie_map\n", err? "FAILED": "PASSED");
     return 0;
 }
